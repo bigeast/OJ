468af19bb1d383762ccd4435de9d3fb40ee95848
bigeast <dadongking@gmail.com>
2025-11-28 02:07:58 +0800

    Huffman AC

--- HDOJ.1053.cc
+++ HDOJ.1053.cc
@@ -11,6 +11,19 @@
 #include <algorithm>
 using namespace std;
 
+// 内存管理真是门学问啊。
+// 一开始是想避免入队出队时候 Node 的不必要拷贝，因此想用 emplace 函数入队，但是这样出队时，还是需要赋值给新的变量，并且合并成新的节点时，是需要指针指向被弹出的Node 对象的。
+// 首先想到的是先开一个 Node 数组，然后队列存储的是 Node*，指向数组中的各个元素，这样的话，优先级队的定义比较复杂，需要用三个参数的声明，而且还需要自定义比较函数。
+// 但是第一个坑就是，数组不能用 vector！因为 vector 是会重新分配内存的，本地运行时，也稳定复现了这个问题，学以致用啊哈哈
+// 修改成普通数组，还是会 WA，调大数组大小，会 MLE。
+// 那就用堆内存，一次性 new 一个 Node 数组出来，但是这样需要手动初始化， 用不到 Node  定义的构造函，代码不简洁。
+// 最终还是用new，但是一次只 new 一个 Node，这样可以用到定义好的构造函数。
+//
+// 还有个坑，就是只有一个元素时，码长是 1 而不是 0, 需要特殊处理。
+//
+// 另外犯的错有，在构造函数的初始化列表里面，right(left)，这种 typo bug 真的很致命。
+//
+// AI 推荐的写法是用智能指针，move 语义，但是写起来太麻烦，不太适合做竞赛题。
 class Node {
   public:
     int val;
@@ -41,13 +54,13 @@ int main() {
       freq[c]++;
     }
     priority_queue<Node*, vector<Node*>, Node::Compare> Q;
-    vector<Node> nodes;
-    nodes.reserve(100000); // 牛逼，值了。用 vector 存储 Node 是不安全的，因为底层会重新分配内存，导致野指针！
+    // vector<Node> nodes;
+    // nodes.reserve(100000); // 牛逼，值了。用 vector 存储 Node 是不安全的，因为底层会重新分配内存，导致野指针！
     int idx = 0;
     for (auto const& kv: freq) {
       // cout << kv.first << " " << kv.second << endl;
-      nodes.emplace_back(kv.second, kv.first);
-      Q.push(&nodes[idx++]);
+      Node *node = new Node(kv.second, kv.first);
+      Q.push(node);
     }
     while (Q.size() > 1) {
       Node* a = Q.top();
@@ -55,8 +68,8 @@ int main() {
       Node* b = Q.top();
       Q.pop();
       // cout << " a " << a->val << " b " << b->val << endl;
-      nodes.emplace_back(a->val + b->val, a, b);
-      Q.push(&nodes[idx++]);
+      Node *node = new Node(a->val + b->val, a, b);
+      Q.push(node);
     }
     unordered_map<char, int> codeLen;
     Node *r = Q.top();
@@ -66,6 +79,11 @@ int main() {
     for (auto const& kv: codeLen) {
       // cout << kv.first << " freq: " << freq[kv.first] << " len: " << kv.second << endl;
       totalLen += kv.second * freq[kv.first];
+    }
+    if (freq.size() == 1) {
+      for (auto const& kv: freq) {
+        totalLen = kv.second;
+      }
     }
     printf("%d %d %.1f\n", (int) str.size() * 8, totalLen, str.size() * 8.0 / totalLen);
   }
