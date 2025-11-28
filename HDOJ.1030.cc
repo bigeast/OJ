2d40b8d6fb951d7ef66902d0002d65d9ad128c0f
bigeast <dadongking@gmail.com>
2025-11-28 12:40:11 +0800

    还是有问题，问题出在那个最后一步的公式不是最优。

--- HDOJ.1030.cc
+++ HDOJ.1030.cc
@@ -21,9 +21,26 @@ pair<int, int> coor(int n) {
   return {r + 1, n - r * r};
 }
 
-pair<int, int> getInterSections(int row, int col, int targetRow) {
-  return {col + 1, 2 * (targetRow - row) + col - 1};
+int getAnsFromUp(int xM, int yM, int xN, int yN) {
+  // 正三角向下求两个交点，然后算曼哈顿距离
+  // 经过 2 * (xN - xM) - 1 步，可到达第一个交点(xN, yM + 1)
+  // (xM, yM) 本层共有 2*xM + 1 个元素，因此后面元素个数是 2*xM + 1 - yM
+  // 平移到 xN 行，再补充一个三角，对称点后面元素个数是 2*xM + 2 - yM
+  // 对称点为 (xN, 2*xN + 1 - (2*xM + 2 - yM)) = (xN, 2 *(xN - xM) + yM - 1)
+  int dx = 2 * (xN - xM) - 1;
+  int y1 = yM + 1;
+  int y2 = 2 * (xN - xM) + yM - 1;
+  int dy = min(abs(yN - y1), abs(yN - y2));
+  return dx + dy;
 }
+
+int getAnsFromDown(int xM, int yM, int xN, int yN) {
+  // 倒三角，可以转化为两个相邻的正三角之后，复用上面的公式，注意 dx 多走了一步要加 1
+  int ans1 = getAnsFromUp(xM, yM - 1, xN, yN);
+  int ans2 = getAnsFromUp(xM, yM + 1, xN, yN);
+  return 1 + min(ans1, ans2);
+}
+
 int main() {
   cin >> M >> N;
   if (M > N) swap(M, N);
@@ -37,31 +54,14 @@ int main() {
   } else {
     int dx, dy;
     if (yM % 2 == 1) { // 正三角
-      // 经过 2 * (xN - xM) - 1 步，可到达第一个交点(xN, yM + 1)
-      // (xM, yM) 本层共有 2*xM + 1 个元素，因此后面元素个数是 2*xM + 1 - yM
-      // 平移到 xN 行，再补充一个三角，对称点后面元素个数是 2*xM + 2 - yM
-      // 对称点为 (xN, 2*xN + 1 - (2*xM + 2 - yM)) = (xN, 2 *(xN - xM) + yM - 1)
-      dx = 2 * (xN - xM) - 1;
-      auto [y1, y2] = getInterSections(xM, yM, xN);
-      dy = min(abs(yN - y1), abs(yN - y2));
-      // cout << "dx " << dx << endl;
-      // cout << "dy " << dy << endl;
-      // cout << "y " << y1 << " " << y2 << endl;
+      ans = 1 + getAnsFromDown(xM + 1, yM + 1, xN, yN);
     } else {
-      // 倒三角，可以转化为两个相邻的正三角之后，复用上面的公式，注意 dx 多走了一步要加 1
       // dx = 2 * (xN - xM)
-      // (xM, yM - 1) -> (xN, yM), ()
-      // (xM, yM + 1)
-      dx = 2 * (xN - xM);
-      auto [y1, y2] = getInterSections(xM, yM - 1, xN);
-      auto [y3, y4] = getInterSections(xM, yM + 1, xN);
-      dy = min(abs(yN - y1), abs(yN - y2));
-      dy = min(dy, min(abs(yN - y3), abs(yN - y4)));
+      ans = getAnsFromDown(xM, yM, xN, yN);
       // cout << "dx " << dx << endl;
       // cout << "dy " << dy << endl;
       // cout << "y " << y1 << " " << y2 << " " << y3 << " " << y4 << endl;
     }
-    ans = dx + dy;
   }
   cout << ans << endl;
 }
