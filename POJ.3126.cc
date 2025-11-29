940fc16cad3eab218a4955a2d37ca3a3b6aa422f
bigeast <dadongking@gmail.com>
2025-11-29 22:21:12 +0800

    Prime Path, BFS

--- POJ.3126.cc
+++ POJ.3126.cc
@@ -6,12 +6,16 @@
 #include <cstring> // pit 4 memset
 using namespace std;
 const int N = 10000;
+
+// 一道简简单单的 BFS 题目，本来是因为偶然看到觉得有意思才做的，结果给我整出来这么多 bug，得好好总结下
+// 主要是这里不再是在图中搜索，不存在坐标问题，导致没有去写判断 valid 的条件，导致 RE 了很久。
+// 还有就是 POJ 前天突然挂了，导致这道题也中断了，今天才好。
 int n;
 int start, target;
 vector<bool> isprime(N, true);
-int visited[N];
+bool visited[N];
 // int pre[N];
-void seiev() {
+void sieve() {
   isprime[0] = isprime[1] = false;
   for (int i = 2; i < sqrt(N) + 1; ++i) {
     if (isprime[i]) {
@@ -21,6 +25,7 @@ void seiev() {
     }
   }
 }
+
 int bfs() {
   queue<int> Q;
   memset(visited, 0, sizeof(visited));
@@ -44,9 +49,10 @@ int bfs() {
         }
         digit %= 10;
         int from = curr - digit * base;
-        if (i == 3) from += base; // pit 3 四位数限制，首位不能为 0
+        // if (i == 3) from += base; // pit 3 四位数限制，首位不能为 0
         for (int j = from; j <= from + 9 * base; j += base) { // pit 1 <=
           if (visited[j] || !isprime[j]) continue;
+          if (j < 1000 || j > 9999) continue; // pit!!!!
           Q.push(j);
           visited[j] = true; // pit 5
           // pre[j] = curr;
@@ -68,13 +74,16 @@ int bfs() {
   depth = -1;
   return depth;
 }
+
 int main() {
-  seiev();
+  sieve();
   cin >> n;
   while(n--) {
     cin >> start >> target;
     int depth = bfs();
     if (depth < 0) cout << "Impossible" << endl;
-    cout << bfs() << endl;
+    else {
+      cout << depth << endl;
+    }
   }
 }
