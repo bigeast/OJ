4aac3e55f0eaa840602a71fdc4b7674a08ed12b5
bigeast <dadongking@gmail.com>
2025-12-14 22:00:29 +0800

    update KMP

--- SPOJ.NAJPF.cpp
+++ SPOJ.NAJPF.cpp
@@ -23,16 +23,18 @@ int main() {
     vector<int> next(p.size());
     next[0] = 0;
     for (int i = 1, j = 0; i < p.size(); ++i) {
-      while (j > 0 && s[i] != s[j]) {
+      while (j > 0 && p[i] != p[j]) {  // 求next数组可以理解为模式串与自身做匹配
         j = next[j - 1];
       }
-      if (s[i] == s[j]) ++j;
+      if (p[i] == p[j]) ++j;
       next[i] = j;
+      // 下标i是单调递增的，且next[i]只依赖于前面已经计算出来的next值，相当于一种特殊的动态规划
     }
     int i = 0, j = 0;
     vector<int> res;
     while (i < s.size()) {
       if (s[i] == p[j]) {
+        // 这里开始写错了，跟next一样自己跟自己去比了，易错点。
         ++i;
         ++j;
       } else {
@@ -43,8 +45,8 @@ int main() {
         }
       }
       if (j == p.size()) {
-        res.push_back(i - j + 1);
-        j = next[j - 1];
+        res.push_back(i - j + 1);  // 题目要求下标从1开始
+        j = next[j - 1];  // 假装还没找到匹配（其实是继续找下一个匹配:D）
       }
     }
     if (res.size() == 0) {
